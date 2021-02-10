c5f9090ea4f126cf4914ec1e11a4afddf548d1af
mblanke70 <blanke@ursulaschule.de>
2021-02-10 22:54:34 +0100

    ...

--- resources/views/wahlfach.blade.php
+++ resources/views/wahlfach.blade.php
@@ -96,7 +96,7 @@
         </div>
 
         <div>
-            <a href="http://fachwahl.ursulaschule.de/qf" class="btn btn-success" role="button">Wahl abgeben</a>
+            <a href="https://fachwahl.ursulaschule.de/qf" class="btn btn-success" role="button">Wahl abgeben</a>
         </div>
 
     @endif
